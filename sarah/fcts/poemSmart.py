1d9c3923a48064dc43d33c08ae40ec457d8e024e
Ning Tu <tuning.cn@gmail.com>
2021-10-30 16:49:39 +0800

    add dir for Jon

---
+++ sarah/fcts/poemSmart.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  7 13:08:01 2019
+
+@author: Ning
+"""
+
+from recitePoem import recitePoem
+from addPoem import addPoem
+
+options = dict([])
+# number of poems to recite
+options['nPrevPoems'] = 4
+# list of poems to review
+options['fileName'] = '../data/poemlist.txt'
+# name of result file, will export CSV file
+options['resultNameBase'] = '../result/result' 
+# whether to show diag info
+options['verbosity'] = 0
+
+# recite poems
+recitePoem(options)
+
+# learn new poem
+while True:
+    learnNew = input('Add new poem?[y/N]')
+    if len(learnNew) == 0:
+        print('No new poem added.')
+        break
+    elif learnNew[0] == 'y':
+        newPoemName = input('Please type the name of the poem you are learning:')
+        if len(newPoemName) == 0:
+            print('No new poem added.')
+            break
+        else:
+            print('New poem title: '+newPoemName)
+            options['newPoemName'] = newPoemName
+            addPoem(options)
+            print('New poem '+newPoemName+' added to your record.')
+    else:
+        print('No new poem added.')
+        break
+print('You are done for today. Good job!')
---
+++ sarah/fcts/recitePoem.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  7 12:49:54 2019
+
+@author: Ning
+"""
+import numpy as np
+import pandas as pd
+import os.path
+import datetime
+import pickle
+
+def recitePoem(options):
+            
+    resultFile = options['resultNameBase']+'.pkl'
+    resultCSV = options['resultNameBase']+'.csv'
+    
+    currTime = datetime.datetime.now()
+    todayDate = currTime.strftime('%Y-%m-%d')
+    
+    # load previous data
+    if os.path.exists(resultCSV):
+        record = pd.read_csv(resultCSV)
+    else:
+        # create initial record file
+        poemDict = readPoemList(options['fileName'])
+        nPoem = len(poemDict)
+        record = pd.DataFrame.from_dict(poemDict, orient = 'index', columns = ['Poem'])
+        record['iPoem'] = record.index
+        record['iPoem'] = pd.to_numeric(record['iPoem'])+1
+        record['nReviews'] = pd.Series(np.zeros(nPoem), index=record.index)
+        record['lastDate'] = pd.Series(todayDate, index=record.index)
+        record['sinceLastDate'] = pd.Series(np.zeros(nPoem), index=record.index)
+        record = record.sort_values('iPoem')
+        cols = ['iPoem','Poem','nReviews','lastDate','sinceLastDate']
+        record = record[cols]
+    
+    # update days since last review
+    for i in record.index:
+        lastDate = record.iloc[i,record.columns.get_loc('lastDate')]
+        record.iloc[i,record.columns.get_loc('sinceLastDate')] = daysBetween(todayDate, lastDate)
+
+    poemIdx = selectPoem(record, options)
+    
+    # update today's review
+    for i in range(len(poemIdx)):
+        record.iloc[poemIdx[i],record.columns.get_loc('nReviews')] += 1
+        record.iloc[poemIdx[i],record.columns.get_loc('lastDate')] = todayDate
+        record.iloc[poemIdx[i],record.columns.get_loc('sinceLastDate')] = 0
+        
+    # save to file
+    with open(resultFile, 'wb') as f:
+        pickle.dump(record, f)
+    record.to_csv(resultCSV, index = False)
+
+def selectPoem(record, options):
+    verbFlag = options['verbosity']
+    nPoemRecite = options['nPrevPoems']
+    if nPoemRecite > 6:
+        print('Gee that is too much! Six titles will be recommended.')
+        nPoemRecite = 6
+    nPoem = len(record.index)
+    nDays = (np.array(record.loc[:,'sinceLastDate']+1))**1
+    nRevs = np.array(record.loc[:,'nReviews'])+0.01
+    prob = nDays/nRevs
+    prob = prob/sum(prob)
+    nPoemShuffle = int(np.ceil(1.0*nPoemRecite))
+    # for starters: if nPoem is less than nPoemShuffle
+    if nPoem < nPoemShuffle:
+        nPoemShuffle = nPoem
+    if nPoem < nPoemRecite:
+        nPoemRecite = nPoem
+    probIdx = np.flip(np.argsort(prob), axis=0)[:nPoemShuffle]
+    if verbFlag:
+        print('Select from '+str(len(probIdx))+' poems.')
+    poemIdx = probIdx[np.random.permutation(len(probIdx))[:nPoemRecite]]
+    if verbFlag:
+        print('Length of recommendation list: '+str(len(poemIdx))+'.')
+    print("Today's "+str(nPoemRecite)+" poem(s) to review:")
+    for i in range(nPoemRecite):
+        print('Poem: '+record.iloc[poemIdx[i],record.columns.get_loc('Poem')])
+    return poemIdx
+
+def daysBetween(d1, d2):
+    d1 = datetime.datetime.strptime(d1, "%Y-%m-%d")
+    d2 = datetime.datetime.strptime(d2, "%Y-%m-%d")
+    return abs((d2 - d1).days)
+
+def readPoemList(filename):
+    poemDict = dict([])
+    contents = open(filename, encoding="utf8").read()
+    poemlist = contents.split('\n')
+    print('You are awesome! You can recite '+str(len(poemlist))+' poems now:')
+    for i in range(len(poemlist)):
+        poemDict[str(i)] = poemlist[i]
+        print('Poem '+str(i+1)+': '+poemlist[i])
+    return poemDict
