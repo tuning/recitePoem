4509cd4ede531b1216d51c5fb70f3226d9805802
Ning Tu <tuning.cn@gmail.com>
2021-11-07 10:39:59 +0800

    remove pkl file for result recording

--- sarah/fcts/addPoem.py
+++ sarah/fcts/addPoem.py
@@ -5,24 +5,19 @@ Created on Sun Sep  8 10:46:43 2019
 
 @author: Ning
 """
-import pickle
+import pandas as pd
 import datetime
 
 def addPoem(options):
     
-    resultFile = options['resultNameBase']+'.pkl'
     resultCSV = options['resultNameBase']+'.csv'
     
     currTime = datetime.datetime.now()
     todayDate = currTime.strftime('%Y-%m-%d')
     
-    with open(resultFile, 'rb') as f:
-        record = pickle.load(f)
-    
+    record = pd.read_csv(resultCSV)
     nRec = len(record.index)
     newRec = {'iPoem':nRec+1,'Poem':options['newPoemName'],'nReviews':0,'lastDate':todayDate,'sinceLastDate':0}
     record = record.append(newRec, ignore_index=True)
     
-    with open(resultFile, 'wb') as f:
-        pickle.dump(record, f)
     record.to_csv(resultCSV, index = False)
